03fe6fd6df1522f6ceba77d67dcac08e7b26f31a
uzair888 <uzairsarwar10@gmail.com>
2025-08-14 00:57:12 +0500

    Fixed the issues metioned in doc

--- app/c/[slugKey]/spletna-stran/steps/step6.js
+++ app/c/[slugKey]/spletna-stran/steps/step6.js
@@ -10,7 +10,12 @@ import toast from "react-hot-toast";
 import { useRouter } from "next/navigation";
 import CompanyPreview from "../components/company-preview";
 
-export default function Step6({ data, onChange, handleStepChange }) {
+export default function Step6({
+  data,
+  onChange,
+  handleStepChange,
+  setIsRender,
+}) {
   const [openBlock, setOpenBlock] = useState(1);
   const router = useRouter();
   const [shops, setShops] = useState([
@@ -189,7 +194,9 @@ export default function Step6({ data, onChange, handleStepChange }) {
                 </div>
               </div>
             </div>
-            {companyId && <CompanyPreview companyData={data} />}
+            {companyId && (
+              <CompanyPreview companyData={data} setIsRender={setIsRender} />
+            )}
           </div>
           <div className="space-y-[8px]">
             <OpenableBlock
